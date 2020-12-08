c385a0157daa1ac1942ea7459f489371cf51d269
Rachel Buchta <rachelbuchta@yahoo.com>
2020-12-08 16:18:39 -0700

    Refactor test file and user.js file

--- src/user.js
+++ src/user.js
@@ -1,20 +1,14 @@
 class User {
-    constructor(userData) {
-        this.id = userData.id
-        this.name = userData.name
-        this.address = userData.address
-        this.email = userData.email
-        this.strideLength = userData.strideLength
-        this.dailyStepGoal = userData.dailyStepGoal
-        this.friends = userData.friends
-    }
-    getFirstName() {
+  constructor(userData) {
+    this.userData = userData
+  }
 
-        const firstName = this.name.split(' ')[0]
-        return firstName[0]
-    }
+  getFirstName() {
+    const firstName = this.userData.name.split(' ')
+    return firstName[0]
+  }
 }
 
 if (typeof module !== 'undefined') {
-    module.exports = User
+  module.exports = User
 }
--- test/user-test.js
+++ test/user-test.js
@@ -5,16 +5,20 @@ const User = require('../src/user')
 const userTestData = require('./test-user-data')
 
 describe('User', () => {
+  let user
 
-    it('should be an instance of User', () => {
-        const user = new User(userTestData[0])
+  beforeEach(function() {
+    user = new User(userTestData[0])
 
-        expect(user).to.be.an.instanceof(User)
-    })
-    it('should return users first name', () => {
+  })
 
-        const user = new User(userTestData[0])
+  it('should be an instance of User', () => {
 
-        expect(user.getFirstName()).to.equal('Luisa')
-    })
+    expect(user).to.be.an.instanceof(User)
+  })
+
+  it('should return users first name', () => {
+
+    expect(user.getFirstName()).to.equal('Luisa')
+  })
 })
