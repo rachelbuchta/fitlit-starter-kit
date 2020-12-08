9624db5595816839aa831d596b3819ab1417794c
Max Bregman <bregmanmax91@gmail.com>
2020-12-08 14:00:38 -0700

    pass class test

--- test/userRepo-test.js
+++ test/userRepo-test.js
@@ -1,2 +1,12 @@
 const chai = require('chai')
-const expect = chai.expect
+const expect = chai.expect;
+
+const UserRepo = require('../src/userRepo')
+
+describe('userRepo', () => {
+    it('should be an instance of UserRepo', () => {
+        const userRepo = new UserRepo()
+
+        expect(userRepo).to.be.an.instanceof(UserRepo)
+    })
+})
