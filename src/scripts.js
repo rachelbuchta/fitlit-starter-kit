8713dc3a43f3b0092325bdf15b75a0b75364f9da
Rachel Buchta <rachelbuchta@yahoo.com>
2020-12-09 16:53:43 -0700

    Query selected calendar days for weekly water and displayed water for each day

--- src/scripts.js
+++ src/scripts.js
@@ -9,13 +9,13 @@ let userHydration = new UserHydration(hydrationData)
     const greeting = document.querySelector('h1')
     const othersStepGoal = document.querySelector('.all-users-step-goal')
     const dailyWater = document.querySelector('.water-ounces')
-    const sundayWater = document.querySelector('water-box7')
-    const mondayWater = document.querySelector('water-box8')
-    const tuesdayWater = document.querySelector('water-box9')
-    const wednesdayWater = document.querySelector('water-box10')
-    const thursdayWater = document.querySelector('water-box11')
-    const fridayWater = document.querySelector('water-box12')
-    const saturdayWater = document.querySelector('water-box13')
+    const sundayWater = document.querySelector('#water-box7')
+    const mondayWater = document.querySelector('#water-box8')
+    const tuesdayWater = document.querySelector('#water-box9')
+    const wednesdayWater = document.querySelector('#water-box10')
+    const thursdayWater = document.querySelector('#water-box11')
+    const fridayWater = document.querySelector('#water-box12')
+    const saturdayWater = document.querySelector('#water-box13')
 
     window.addEventListener('load', displayUserInfo)
 
@@ -26,6 +26,7 @@ let userHydration = new UserHydration(hydrationData)
       displayUserStrideLength()
       displayAllUsersStepGoal()
       displayDailyWaterIntake()
+      displayWeeklyWaterIntake()
     }
 
     function displayGreeting() {
@@ -49,5 +50,16 @@ let userHydration = new UserHydration(hydrationData)
 
     function displayDailyWaterIntake() {
 
-      dailyWater.innerHTML = `${userHydration.returnDailyConsumption("2019/06/15")} oz.`
+      dailyWater.innerHTML = `${userHydration.returnDailyConsumption("2019/06/15")} OZs.`
+    }
+
+    function displayWeeklyWaterIntake() {
+      console.log(userHydration.returnWeeklyConsumption(1)['Sunday'])
+      sundayWater.innerText = userHydration.returnWeeklyConsumption(1).Sunday
+      mondayWater.innerText = userHydration.returnWeeklyConsumption(1).Monday
+      tuesdayWater.innerText = userHydration.returnWeeklyConsumption(1).Tuesday
+      wednesdayWater.innerText = userHydration.returnWeeklyConsumption(1).Wednesday
+      thursdayWater.innerText = userHydration.returnWeeklyConsumption(1).Thursday
+      fridayWater.innerText = userHydration.returnWeeklyConsumption(1).Friday
+      saturdayWater.innerText = userHydration.returnWeeklyConsumption(1).Saturday
     }
