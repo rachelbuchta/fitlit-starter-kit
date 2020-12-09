5560b0860ab3842006b9ac001e838a17a578baaf
Rachel Buchta <rachelbuchta@yahoo.com>
2020-12-08 20:25:03 -0700

    Add userHydration file and avgOuncesConsumed method

--- src/userHydration.js
+++ src/userHydration.js
@@ -1,6 +1,18 @@
 class UserHydration {
-  constructor() {
+  constructor(data) {
+    this.data = data
+    this.id = data.userID
+    this.date = data.date
+    this.numOunces = data.numOunces
+  }
 
+  avgOuncesConsumed(id) {
+    const userWeek = this.data.filter(day => day.userID == id)
+    const totalOunces = userWeek.reduce((acc, day) => {
+     acc += day.numOunces
+     return acc
+    },0)
+    return Math.round(totalOunces / userWeek.length)
   }
 }
 
--- test/userHydration-test.js
+++ test/userHydration-test.js
@@ -9,26 +9,26 @@ describe('UserHydration', () => {
   let userHydration
 
   beforeEach(function() {
-    userHydration = new UserHydration()
+    userHydration = new UserHydration(userHydrationData)
 
   })
 
-  it.skip('should be an instance of UserHydration', () => {
+  it('should be an instance of UserHydration', () => {
 
     expect(userHydration).to.be.an.instanceof(UserHydration)
   })
 
-  it.skip('should calculate all time average ounces of water consumed per day', () => {
+  it('should calculate all time average ounces of water consumed per day', () => {
 
-    expect(userHydration.avgOuncesConsumed(1)).to.equal(454)
+    expect(userHydration.avgOuncesConsumed(1)).to.equal(65)
   })
 
-  it.skip('should return amount of ounces consumed in a day by date' => {
+  it.skip('should return amount of ounces consumed in a day by date', () => {
 
     expect(userHydration.returnDailyConsumption("2019/06/15")).to.equal(37)
   })
 
-  it.skip('should return amount of ounces consumed in a week for each day' => {
+  it.skip('should return amount of ounces consumed in a week for each day', () => {
 
     expect(userHydration.returnWeeklyConsumption()).to.equal({Sunday: 50, Monday: 37, Tuesday: 69, Wednesday: 96, Thursday: 61, Friday: 91, Saturday: 50})
   })
