52c4c22f92c5f0742d011963058fb0c8799fd7b8
Max Bregman <bregmanmax91@gmail.com>
2020-12-09 19:53:28 -0700

    make sleep  cclass tests

--- src/userHydration.js
+++ src/userHydration.js
@@ -1,43 +1,46 @@
 class UserHydration {
-  constructor(data) {
-    this.data = data
-    this.id = data.userID
-    this.date = data.date
-    this.numOunces = data.numOunces
-  }
+    constructor(data) {
+        this.data = data
+        this.id = data.userID
+        this.date = data.date
+        this.numOunces = data.numOunces
+    }
 
-  avgOuncesConsumed(id) {
-    const userWeek = this.data.filter(day => day.userID == id)
-    const totalOunces = userWeek.reduce((acc, day) => {
-     acc += day.numOunces
-     return acc
-    },0)
-    return Math.round(totalOunces / userWeek.length)
-  }
+    avgOuncesConsumed(id) {
+        const userWeek = this.data.filter(day => day.userID == id)
+        const totalOunces = userWeek.reduce((acc, day) => {
+            acc += day.numOunces
+            return acc
+        }, 0)
+        return Math.round(totalOunces / userWeek.length)
+    }
 
-  returnDailyConsumption(date) {
-    const day = this.data.find(day => day.date === date)
-    return day.numOunces
-  }
+    returnDailyConsumption(date) {
+        const day = this.data.find(day => day.date === date)
+        return day.numOunces
+    }
 
-  returnWeeklyConsumption(id) {
-    const currentUser = this.data.filter(day => day.userID == id)
-    const weeklyIntake = {Sunday: currentUser[0].numOunces,
-     Monday: currentUser[1].numOunces,
-     Tuesday: currentUser[2].numOunces,
-     Wednesday: currentUser[3].numOunces,
-     Thursday: currentUser[4].numOunces,
-     Friday: currentUser[5].numOunces,
-     Saturday: currentUser[6].numOunces}
-     return weeklyIntake
+    returnWeeklyConsumption(id) {
+        const currentUser = this.data.filter(day => day.userID == id)
+        const weeklyIntake = {
+            Sunday: currentUser[currentUser.length - 7].numOunces,
+            Monday: currentUser[currentUser.length - 6].numOunces,
+            Tuesday: currentUser[currentUser.length - 5].numOunces,
+            Wednesday: currentUser[currentUser.length - 4].numOunces,
+            Thursday: currentUser[currentUser.length - 3].numOunces,
+            Friday: currentUser[currentUser.length - 2].numOunces,
+            Saturday: currentUser[currentUser.length - 1].numOunces
+        }
+        console.log(weeklyIntake);
+        return weeklyIntake
 
-    // currentUser.reduce((acc, day) => {
-    //   console.log(acc.Monday = currentUser.numOunces)
-    //   return acc
-    // }, {})
-  }
+        // currentUser.reduce((acc, day) => {
+        //   console.log(acc.Monday = currentUser.numOunces)
+        //   return acc
+        // }, {})
+    }
 }
 
-if(typeof module !== 'undefined') {
-  module.exports = UserHydration
+if (typeof module !== 'undefined') {
+    module.exports = UserHydration
 }
