d29e68ec95a412775429494ea3526275dd8e8911
Max Bregman <bregmanmax91@gmail.com>
2020-12-12 15:18:39 -0700

    add calculate miles walked method

--- src/userActivity.js
+++ src/userActivity.js
@@ -1,10 +1,43 @@
 /* eslint-disable max-len */
-class UserActivity {
-  constructor() {
 
-  }
+const UserRepo = require('./userRepo')
+const userTestData = require('../test/test-user-data')
+let userRepo = new UserRepo(userTestData)
+
+class UserActivity {
+    constructor(data) {
+        this.data = data
+        this.currentUser
+
+    }
+
+    findCurrentUser(id) {
+        this.currentUser = userRepo.getUserData(id)
+    }
+
+    calculateMilesWalked(date) {
+        const strideLength = this.currentUser.strideLength
+        const numberOfSteps = this.data.filter(day => day.date === date)
+            .find(user => user.userID === this.currentUser.id).numSteps
+        const milesWalked = ((strideLength * numberOfSteps) / 5280).toFixed(1)
+        return parseFloat(milesWalked)
+    }
 }
 
 if (typeof module !== 'undefined') {
-  module.exports = UserActivity
+    module.exports = UserActivity
 }
+
+
+
+
+
+
+
+
+// const newUserRepo = new UserRepo(userTestData)
+// const newActivityRepo = new UserRepo(acivityTestData)
+
+//         const milesWalked = ((newUserRepo.getUserData(id).strideLength * numberOfSteps.getUserData(id).numSteps) / 5280).toFixed(1)
+//         return parseFloat(milesWalked)
+//     }
--- test/userActivity-test.js
+++ test/userActivity-test.js
@@ -8,102 +8,103 @@ const UserRepo = require('../src/userRepo')
 const userTestData = require('./test-user-data')
 
 describe('UserActivity', () => {
-  let userActivity
-  let userRepo
+    let userActivity
+    let userRepo
 
-  beforeEach(function() {
-    userActivity = new UserActivity(userActivityData)
-    userRepo = new UserRepo(userTestData)
+    beforeEach(function() {
+        userActivity = new UserActivity(userActivityData)
+        userRepo = new UserRepo(userTestData)
 
-  })
+    })
 
-  it.only('should be an instance of UserActivity', () => {
-    console.log(userRepo.getUserData(1))
+    it('should be an instance of UserActivity', () => {
 
-    expect(userActivity).to.be.an.instanceof(UserActivity)
 
-  })
+        expect(userActivity).to.be.an.instanceof(UserActivity)
 
-  it('should calculate the amount of miles a user has walked by a given day', () => {
+    })
 
-    userRepo.getUserData(1)
-    //strideLength = 4.3
+    it.only('should calculate the amount of miles a user has walked by a given day', () => {
 
-    expect(userActivity.calculateMilesWalked("2019/06/15")).to.equal()
 
-  })
+        userActivity.findCurrentUser(1)
 
-  it('should return the number of steps taken on a given day', () => {
 
-    expect(userActivity.returnNumberOfSteps(1, "2019/06/15")).to.equal(3577)
+        expect(userActivity.calculateMilesWalked("2019/06/15")).to.equal(2.9)
 
-  })
+    })
 
-  it('should return how many minutes active a user was for a given day', () => {
+    it.skip('should return the number of steps taken on a given day', () => {
 
-    expect(userActivity.returnMinutesActive(1, "2019/06/15")).to.equal(140)
+        expect(userActivity.returnNumberOfSteps(1, "2019/06/15")).to.equal(3577)
 
-  })
+    })
 
-  it('should calculate average minutes active for a given week by date and ID', () => {
+    it.skip('should return how many minutes active a user was for a given day', () => {
 
-    expect(userActivity.calculateAvgMinByWeek(1, "2019/06/15")).to.equal(171)
+        expect(userActivity.returnMinutesActive(1, "2019/06/15")).to.equal(140)
 
-  })
+    })
 
-  it('should return true if a user exceeded their step goal for a given date', () => {
+    it.skip('should calculate average minutes active for a given week by date and ID', () => {
 
-    userRepo.getUserData(1)
+        expect(userActivity.calculateAvgMinByWeek(1, "2019/06/15")).to.equal(171)
 
-    expect(userActivity.exceedStepGoalCheck(1, "2019/06/15")).to.equal(false)
+    })
 
-  })
+    it.skip('should return true if a user exceeded their step goal for a given date', () => {
 
-  it('should return all days where a user has exceeded their step goal', () => {
+        userRepo.getUserData(1)
 
-    userRepo.getUserData(1)
+        expect(userActivity.exceedStepGoalCheck(1, "2019/06/15")).to.equal(false)
 
-    expect(userActivity.getExceededStepDays(1)).to.deep.equal(["2019/06/17", "2019/06/20"])
+    })
 
-  })
+    it.skip('should return all days where a user has exceeded their step goal', () => {
 
-  it('should return a users all time stair climbing record', () => {
+        userRepo.getUserData(1)
 
-    expect(userActivity.findStairRecord(1)).to.equal(36)
+        expect(userActivity.getExceededStepDays(1)).to.deep.equal(["2019/06/17", "2019/06/20"])
 
-  })
+    })
 
-  it('should calculate average number of steps taken for all users on a given date', () => {
+    it.skip('should return a users all time stair climbing record', () => {
 
-    expect(userActivity.calculateAvgTotalSteps("2019/06/15")).to.equal(5091)
+        expect(userActivity.findStairRecord(1)).to.equal(36)
 
-  })
+    })
 
-  it('should calculate average stairs climbed by all users for a given date', () => {
+    it.skip('should calculate average number of steps taken for all users on a given date', () => {
 
-    expect(userActivity.calculateAvgStairsClimbed("2019/06/15")).to.equal(20)
+        expect(userActivity.calculateAvgTotalSteps("2019/06/15")).to.equal(5091)
 
-  })
+    })
 
-  it('should calculate average number of minutes active between all users for a given date', () => {
+    it.skip('should calculate average stairs climbed by all users for a given date', () => {
 
-    expect(userActivity.calculateAvgMinActive("2019/06/15")).to.equal(131)
+        expect(userActivity.calculateAvgStairsClimbed("2019/06/15")).to.equal(20)
 
-  })
+    })
 
-  it('should return minutes active each day for a week by date', () => {
+    it.skip('should calculate average number of minutes active between all users for a given date', () => {
 
-    expect(userActivity.getDataByWeek(1, '2019/06/15', "sleepQuality")).to.deep.equal([140, 175, 168, 165, 275, 140, 135])
-  })
+        expect(userActivity.calculateAvgMinActive("2019/06/15")).to.equal(131)
 
-  it('should return number of steps each day for a week by date', () => {
+    })
 
-    expect(userActivity.getDataByWeek(1, '2019/06/15', "sleepQuality")).to.deep.equal([3577, 6637, 14329, 4419, 8429, 14478, 6760])
-  })
+    it.skip('should return minutes active each day for a week by date', () => {
 
-  it('should return number of miles active each day for a week by date', () => {
+        expect(userActivity.getDataByWeek(1, '2019/06/15', "sleepQuality")).to.deep.equal([140, 175, 168, 165, 275, 140, 135])
+    })
 
-    expect(userActivity.getDataByWeek(1, '2019/06/15', "sleepQuality")).to.deep.equal([])
-  })
+    it.skip('should return number of steps each day for a week by date', () => {
+
+        expect(userActivity.getDataByWeek(1, '2019/06/15', "sleepQuality")).to.deep.equal([3577, 6637, 14329, 4419, 8429, 14478, 6760])
+    })
+
+    it.skip('should return number of miles active each day for a week by date', () => {
+
+        expect(userActivity.getDataByWeek(1, '2019/06/15', "sleepQuality")).to.deep.equal([])
+    })
 
 })
